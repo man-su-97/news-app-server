378aa9a28fb283606c4c36d80daaabfbc18ba569
Arindam Chowdhury <carindam24@gmail.com>
2026-04-08 13:11:44 +0530

    FEAT: implement async repository layer with rollback support and role-based access control for User, RefreshToken, OTP, AccessLog entity

---
+++ auth-service/app/repositories/otp_repo.py
@@ -0,0 +1,71 @@
+import logging
+from datetime import datetime, timezone
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import select
+from app.models.otp import OTP
+from app.core.enums import OTPType
+
+logger = logging.getLogger(__name__)
+
+class OTPRepository:
+  def __init__(self, db: AsyncSession) -> None:
+    self.db = db
+
+  async def create_otp(self, user_id: int, otp_code:str, otp_type: OTPType, expires_at: datetime):
+    try:
+      otp_obj = OTP(
+        user_id=user_id,
+        otp_code=otp_code,
+        otp_type=otp_type,
+        expires_at=expires_at
+      )
+      self.db.add(otp_obj)
+      await self.db.commit()
+      await self.db.refresh(otp_obj)
+      return otp_obj
+    except SQLAlchemyError:
+      logger.exception("Database error occurred while creating otp.")
+      raise
+    except Exception as e:
+      logger.exception(f"AN UNEXPECTED BUG OCCURRED in create_otp!\n\n{e}")
+      self.db.rollback()
+      raise
+
+  async def get_valid_otp(self, user_id: int, otp_code: str, otp_type: OTPType):
+    try:
+      result = await self.db.execute(
+        select(OTP).where(
+          OTP.user_id == user_id,
+          OTP.otp_code == otp_code,
+          OTP.otp_type == otp_type,
+          OTP.is_used == False,
+          OTP.expires_at > datetime.now(timezone.utc)
+        )
+      )
+      return result.scalar_one_or_none()
+    except SQLAlchemyError:
+      logger.exception("Database error occurred while fetching otp.")
+      raise
+    except Exception as e:
+      logger.exception(f"AN UNEXPECTED BUG OCCURRED in get_valid_otp!\n\n{e}")
+      self.db.rollback()
+      raise
+
+  async def mark_otp_used(self, otp_id: int):
+    try:
+      result = await self.db.execute(select(OTP).where(OTP.id == otp_id))
+      otp = result.scalar_one_or_none()
+      if not otp:
+        return False
+      otp.is_used = True
+      await self.db.commit()
+      await self.db.refresh(otp)
+      return True
+    except SQLAlchemyError:
+      logger.exception("Database error occurred while marking otp as used.")
+      raise
+    except Exception as e:
+      logger.exception(f"AN UNEXPECTED BUG OCCURRED in mark_otp_used!\n\n{e}")
+      self.db.rollback()
+      raise
---
+++ auth-service/app/repositories/user_repo.py
@@ -0,0 +1,103 @@
+import logging
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.models.users import User
+from app.core.enums import UserStatus, UserRole
+
+logger = logging.getLogger(__name__)
+
+class UserRepository:
+  def __init__(self, db: AsyncSession) -> None:
+    self.db = db
+
+  async def create(self,
+    email: str,
+    password_hash: str,
+    full_name: str,
+    is_verified: bool,
+    status: str,
+    role: UserRole = UserRole.USER
+  ):
+    try:
+      user = User(
+        email=email,
+        password_hash=password_hash,
+        full_name=full_name,
+        is_verified=is_verified,
+        role=role,
+        status=status
+      )
+      self.db.add(user)
+      await self.db.commit()
+      await self.db.refresh(user)
+      return user
+    except Exception as e:
+      logger.error(f"Error creating user: {e}")
+      self.db.rollback()
+      raise e
+    
+  async def get_by_email(self, email: str):
+    try:
+      result = await self.db.execute(select(User).where(User.email == email))
+      return result.scalar_one_or_none()
+    except Exception as e:
+      logger.error(f"Error getting user by email: {e}")
+      self.db.rollback()
+      raise e
+    
+  async def get_by_id(self, user_id: int):
+    try:
+      result = await self.db.execute(select(User).where(User.id == user_id))
+      return result.scalar_one_or_none()
+    except Exception as e:
+      logger.error(f"Error getting user by id: {e}")
+      self.db.rollback()
+      raise e
+    
+  async def update_user_password(self, user_id: int, password_hash: str):
+    try:
+      result = await self.db.execute(select(User).where(User.id == user_id))
+      user = result.scalar_one_or_none()
+      if not user:
+        logger.error(f"User with id {user_id} not found")
+        return None
+      user.password_hash = password_hash
+      await self.db.commit()
+      await self.db.refresh(user)
+      return user
+    except Exception as e:
+      logger.error(f"Error updating user password: {e}")
+      self.db.rollback()
+      raise e
+    
+  async def mark_user_verified(self, user_id: int):
+    try:
+      result = await self.db.execute(select(User).where(User.id == user_id))
+      user = result.scalar_one_or_none()
+      if not user:
+        logger.error(f"User with id {user_id} not found")
+        return None
+      user.is_verified = True
+      await self.db.commit()
+      await self.db.refresh(user)
+      return user
+    except Exception as e:
+      logger.error(f"Error marking user as verified: {e}")
+      self.db.rollback()
+      raise e
+    
+  async def update_user_status(self, user_id: int, status: UserStatus):
+    try:
+      result = await self.db.execute(select(User).where(User.id == user_id))
+      user = result.scalar_one_or_none()
+      if not user:
+        logger.error(f"User with id {user_id} not found")
+        return None
+      user.status = status
+      await self.db.commit()
+      await self.db.refresh(user)
+      return user
+    except Exception as e:
+      logger.error(f"Error updating user status: {e}")
+      self.db.rollback()
+      raise e
